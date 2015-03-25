9d2efd5490f5d176103d484326e6be1f28b0eb6e
CoderSparks <codersparks@gmail.com>
2015-03-25 20:29:15 +0100

    Commit to move pcs

--- src/main/java/spittr/config/SpittrWebAppInitializer.java
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -1,5 +1,8 @@
 package spittr.config;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpittrWebAppInitializer extends
@@ -19,6 +22,11 @@ public class SpittrWebAppInitializer extends
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement("C:/temp/"));
 	}
 
 }
